e1a7698c48e3053b26ec8dc29664dd117c89af02
tsengjonathan <tseng.j@husky.neu.edu>
2019-07-27 09:43:30 -0400

    Install bootstrap and initialize .scss

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './custom.scss';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
