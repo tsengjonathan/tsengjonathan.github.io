0d60d2cf4546612fd002b32597805596ab8a08cc
tsengjonathan <tseng.j@husky.neu.edu>
2019-07-27 18:08:27 -0400

    Rename .js files with .jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="container">
+      <Navbar title="Home" />
+      <h1 className="text-monospace text-uppercase text-white text-center">Jonathan Tseng</h1>
+    </div>
+  );
+}
+
+export default App;
